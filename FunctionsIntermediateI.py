872ed3149e462569a091917858f096b20f645e11
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-08 18:58:10 -0500

    4 Done

--- FunctionsIntermediateI.py
+++ FunctionsIntermediateI.py
@@ -12,8 +12,22 @@ sports_directory = {
 z = [ {'x': 10, 'y': 20} ]
 
 # 1 Change the value 10 in x to 15. Once you're done, x should now be [ [5,2,3], [15,8,9] ].
-
+print()
+x[1][0] = 15
+print(x)
 
 # 2 Change the last_name of the first student from 'Jordan' to 'Bryant'
+print()
+print(students[0]['first_name'])
+students[0]['first_name'] = "Bryant"
+print(students[0]['first_name'])
+
 # 3 In the sports_directory, change 'Messi' to 'Andres'
+print()
+print(sports_directory['soccer'][0])
+sports_directory['soccer'][0] = "Andres"
+print(sports_directory['soccer'][0])
+
 # 4 Change the value 20 in z to 30
+print()
+print()
