dcda8fa2d6d138b9e398ba69ea225c7cffd4540b
=Eric Niemeyer <=niemeyer.eric@gmail.com>
2022-02-08 19:11:50 -0500

    2nd Done

--- FunctionsIntermediateI.py
+++ FunctionsIntermediateI.py
@@ -33,3 +33,34 @@ print()
 print(z[0]['y'])
 z[0]['y'] = 30
 print(z[0]['y'])
+
+
+# ///////////////////////////////
+
+
+# 2 Iterate Through a List of Dictionaries
+
+students2 = [
+         {'first_name':  'Michael', 'last_name' : 'Jordan'},
+         {'first_name' : 'John', 'last_name' : 'Rosales'},
+         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+         {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+
+# should output: (it's okay if each key-value pair ends up on 2 separate lines;
+# bonus to get them to appear exactly as below!)
+
+# first_name - Michael, last_name - Jordan
+# first_name - John, last_name - Rosales
+# first_name - Mark, last_name - Guillen
+# first_name - KB, last_name - Tonel
+
+# Create a function iterateDictionary(some_list) that, given a list of dictionaries, the function loops through each dictionary in the list and prints each key and the associated value. For example, given the following list:
+print()
+
+def iterateDictionary(list):
+    for i in range (0, len(list)):
+        print(f"first_name - {list[i]['first_name']}, last_name - {list[i]['last_name']}")
+    
+
+iterateDictionary(students2)
